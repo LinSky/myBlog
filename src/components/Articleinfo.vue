f98039e36b32a2bca4735850033ff7df93500b69
liling <397588184@qq.com>
2016-09-18 16:50:38 +0800

    测试API

--- src/components/Articleinfo.vue
+++ src/components/Articleinfo.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="item" v-for="item in items">
       <div class="thumbnail">
-          <img v-bind:src="item.img" alt="" />
+          <img v-bind:src="item.thumb" alt="" />
       </div>
       <h2>{{item.title}}</h2>
+      <div class="time">{{new Date(item.createTime).getFullYear() +'-'+ (new Date(item.createTime).getMonth()+1) + '-' + new Date(item.createTime).getDate() + ' ' + new Date(item.createTime).getHours()+':'+new Date(item.createTime).getMinutes() + ':' + new Date(item.createTime).getSeconds()}}</div>
+      <em></em>
   </div>
 </template>
 
@@ -19,7 +21,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
 .item{
-    background-color: #FFF; margin-bottom: 40px; box-shadow: 0 0 5px #888;
+    background-color: #FFF; margin-bottom: 40px; box-shadow: 0 0 5px #888; position: relative;
     .thumbnail{
         height: 300px; overflow: hidden;
         img{
@@ -28,6 +30,12 @@ export default {
     }
     h2{
         padding: 0 20px; line-height: 48px; color: #333; font-size: 24px;
+    }
+    .time{
+        position: absolute; top: 0; left: -240px; font-size: 16px; color: #FFF; line-height: 24px; text-shadow:  0 0 2px #999;
+    }
+    em{
+        position: absolute; top: 0; left: -51px; background-color: #FFF; border-radius: 50%; width: 20px; height: 20px; box-shadow:  0 0 2px #999;
     }
 }
 </style>
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -13,20 +13,19 @@ export default {
     'articleinfo': Articleinfo
   },
   data () {
-    let articlesData = [
-      {
-        title: '文章标题测试一',
-        img: 'http://imglf1.nosdn.127.net/img/TnhVL3ZGTmh0cDNKOHZIUlNONFlmTzVsZ1k5SVBIcmQzSkI0TUlILzhwcFJ6Yk84YTg2QzZRPT0.jpg?imageView&thumbnail=500x0&quality=96&stripmeta=0&type=jpg',
-        time: '2016-09-07 15:30'
-      },
-      {
-        title: '文章标题测试2',
-        img: 'http://imglf2.nosdn.127.net/img/TnhVL3ZGTmh0cDNIcm9nRk9EWlZ2c2VPQ2dzTHRoZG5YK2UwRnRVNXEyVmdtQ1JZWkJSQjFnPT0.jpg?imageView&thumbnail=500x0&quality=96&stripmeta=0&type=jpg',
-        time: '2016-09-07 15:30'
-      }
-    ]
     return {
-      articles: articlesData
+      articles: []
+    }
+  },
+  ready () {
+    this.getArticles()
+  },
+  methods: {
+    getArticles: function () {
+      this.$http.get('http://127.0.0.1:3000/articles')
+        .then((response) => {
+          this.$set('articles', response.data)
+        })
     }
   }
 }
@@ -34,7 +33,7 @@ export default {
 
 <style lang="less" scoped>
 .acticles_list{
-    width: 600px; margin: auto;
+    width: 600px; padding: 40px 0 40px 40px; margin: auto; border-left: rgba(255, 255, 255, .2) solid 4px;
 }
 
 </style>
